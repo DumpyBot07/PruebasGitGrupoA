65eb8fb9f0279389284e198699dcf4dad0233d97
Diego777p <154026896+Diego777p@users.noreply.github.com>
2023-12-19 08:44:15 -0600

    tipo d mascotas
    avance

--- pruebasGit/Program.cs
+++ pruebasGit/Program.cs
@@ -13,3 +13,40 @@ public interface IMascota
     void CambiarDueño(string nuevoDueño); 
 }
 
+// Enumerations for species and temperament
+public enum Especie
+{
+    Perro,
+    Gato,
+    Capibara
+}
+
+public enum Temperamento
+{
+    Amable,
+    Nervioso,
+    Agresivo
+}
+
+// tipo de papumascota
+public abstract class Mascota : IMascota
+{
+    private static int contadorGlobal = 0;
+
+    public string Id { get; }
+    public string Nombre { get; set; }
+    private int _edad;
+    public int Edad
+    {
+        get { return _edad; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Age cannot be negative.");
+            }
+            _edad = value;
+        }
+    }
+    public Temperamento Temperamento { get; set; }
+    public string Dueño { get; set; }
