c46c0f4db3240310cecd19ec0da30395133479ce
DumpyBot07 <141478520+DumpyBot07@users.noreply.github.com>
2023-12-19 08:36:01 -0600

    Interfaz de mascota
    agrega id nombre edad temperamento dueño y dos metodos hacer ruido y cambiar dueño

--- pruebasGit/Program.cs
+++ pruebasGit/Program.cs
@@ -1,11 +1,15 @@
-﻿namespace pruebasGit
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hola");
+﻿using System;
 
-        }
-    }
+// Interface for a pet
+public interface IMascota
+{
+    string Id { get; } 
+    string Nombre { get; set; } 
+    int Edad { get; set; } 
+    Temperamento Temperamento { get; set; } 
+    string Dueño { get; set; } 
+
+    void HacerRuido(); 
+    void CambiarDueño(string nuevoDueño); 
 }
+
