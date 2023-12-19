a736158c608aebfb7c426d967fb5878e2fe0bac1
DumpyBot07 <141478520+DumpyBot07@users.noreply.github.com>
2023-12-19 09:37:28 -0600

    clase persona y menu admin center
    papu

--- pruebasGit/Program.cs
+++ pruebasGit/Program.cs
@@ -1,5 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+public class Person
+{
+    private static int counter = 0;
+    public string Id {get; }
+    public string Name{get; set;}
+    public Person(string name){
+        counter++;
+        Id = $"Person{counter}";
+        Name = name;
+    }
+} 
 // Interfaz para mascota
 public interface IMascota
 {
@@ -93,15 +106,17 @@ public class Perro : Mascota
     {
         Console.WriteLine("Grrrr");
     }
+
 }
 
 //El metodo principal para ejecutar el codigo
 class Program
 {
+    static List<Person> registeredPersons = new List<Person>();
     static void Main()
     {
         // uso de ejemplos
-        Perro miPerro = new Perro("Firulais", 5, Temperamento.Amable, "Juan");
+        Perro miPerro = new Perro("Pugnacio", 5, Temperamento.Amable, "Jose");
         miPerro.HacerRuido(); // Escribe el sonido del animal
         miPerro.MoverCola(); // Llama al metodo para que el perro mueve la cola
         miPerro.CambiarDueño("Armando"); // metodo de la mascota Firulais ha cambiado su dueño a x persona
@@ -122,10 +137,10 @@ class Program
                     AdminCenter();
                     break;
                 case "2":
-                    // impementear adopciones ADMINI
+                    // implementear adopciones ADMINI
                     break;
                 case "3":
-                    // implementarr bienestar de animalotes ADMINI
+                    // implementar bienestar de animales ADMIN
                     break;
                 case "4":
                     // interaccion simulaciones 
@@ -138,4 +153,31 @@ class Program
             }
         }
     }
+    static void AdminCenter()
+    {
+        while (true)
+        {
+            Console.WriteLine("1 - Administración de personas");
+            Console.WriteLine("2 - Administración de mascotas");
+            Console.WriteLine("3 - Regresar a menú anterior");
+
+            var option = Console.ReadLine();
+
+            switch (option){
+                case "1":
+                AdminPeople();
+                    break;
+                case "2":
+                    // Implementa administracion de animales
+                    break;
+                case "3":
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
+       
+}
 
