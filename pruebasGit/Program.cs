cb7c2ec5e4a17c576e0e06d383334c3746c33d5b
Diego777p <154026896+Diego777p@users.noreply.github.com>
2023-12-19 08:23:54 -0600

    prueba
    prueba fast

--- pruebasGit/Program.cs
+++ pruebasGit/Program.cs
@@ -9,3 +9,10 @@
         }
     }
 }
+menú principal con las siguientes opciones
+1 - Administración del centro
+2 - Administración de adopciones
+3 - Administración de bienestar animal
+4 - Simulación de interacciones
+4 - Finalizar programa
+
